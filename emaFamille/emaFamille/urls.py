2e39850a6483abe386eebf08ca8d63659649a87a
NapsyGit <alban.bertieaux@gmail.com>
2023-03-09 11:47:40 +0100

    formater

--- emaFamille/emaFamille/urls.py
+++ emaFamille/emaFamille/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from homeApp import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -26,7 +27,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_user, name='logout'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('presentation', views.presentation, name= 'presentation'),
+    path('presentation', views.presentation, name='presentation'),
+    path('inscription', views.inscription, name='inscription'),
     path('forum', views.forum, name='forum')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
